3af6b416c0d491d190625b8a45f265ceb087b15c
bchh325 <bbchhorb@gmail.com>
2025-03-23 05:18:49 -0700

    routing

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -6,13 +6,13 @@ import { Button } from '../ui/button'
 import { useRouter } from 'next/navigation'
 
 export default function NavBar() {
-  const [currentRoute, setCurrentRoute] = useState("Route One")
+  const [currentRoute, setCurrentRoute] = useState("dashboard")
   const router = useRouter()
 
 
   const handleRoute = (route: string) => {
     setCurrentRoute(route)
-    //router.push("/assignments")
+    router.push("/" + route)
   }
 
   return (
@@ -20,10 +20,10 @@ export default function NavBar() {
       <div className={styles.top}>
         <div className={styles.logo}>Logo</div>
         <div className={styles.buttons}>
-          <Button style={{ border: "none", boxShadow: "none" }} onClick={() => { handleRoute("Route One") }} variant={currentRoute == "Route One" ? undefined : "outline"}>Route One</Button>
-          <Button style={{ border: "none", boxShadow: "none" }} onClick={() => { handleRoute("Route Two") }} variant={currentRoute == "Route Two" ? undefined : "outline"}>Route Two</Button>
-          <Button style={{ border: "none", boxShadow: "none" }} onClick={() => { handleRoute("Route Three") }} variant={currentRoute == "Route Three" ? undefined : "outline"}>Route Three</Button>
-          <Button style={{ border: "none", boxShadow: "none" }} onClick={() => { handleRoute("Route Four") }} variant={currentRoute == "Route Four" ? undefined : "outline"}>Route Four</Button>
+          <Button style={{ border: "none", boxShadow: "none" }} onClick={() => { handleRoute("dashboard") }} variant={currentRoute == "dashboard" ? undefined : "outline"}>Dashboard</Button>
+          <Button style={{ border: "none", boxShadow: "none" }} onClick={() => { handleRoute("productivity") }} variant={currentRoute == "productivity" ? undefined : "outline"}>Productivity</Button>
+          <Button style={{ border: "none", boxShadow: "none" }} onClick={() => { handleRoute("calendar") }} variant={currentRoute == "calendar" ? undefined : "outline"}>Calendar</Button>
+          <Button style={{ border: "none", boxShadow: "none" }} onClick={() => { handleRoute("insights") }} variant={currentRoute == "insights" ? undefined : "outline"}>Insights</Button>
         </div>
       </div>
       <div className={styles.bottom}>
